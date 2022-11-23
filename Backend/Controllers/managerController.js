22e6543eef318b9351ad3eb902a8910426571e82
sabirkhaloufi <sabirkhaloufi@gmail.com>
2022-11-23 14:41:28 +0100

    dockerize my application

--- Backend/Controllers/managerController.js
+++ Backend/Controllers/managerController.js
@@ -6,7 +6,6 @@ const RoleModel = require("../Models/RoleModel")
 const GetManager =  async(req,res) => {
     let user = req.user;
     user.role = await RoleModel.findOne({_id: user.role[0]})
-
     res.json(user)
 }
 
